e12ed790336a7ecaf4b6b988a2c56be7e0f01781
prudviblackpanther <prudvinaidum03@gmail.com>
2024-03-26 19:36:56 +0530

    javascript

---
+++ Javascript/javascript.js
@@ -0,0 +1,39 @@
+console.log("helo  world");
+console.log(2+4);
+const pi=3.1415;
+var x=1;
+console.log(x)
+var y=1.4;
+console.log(y);
+var z="hello";
+console.log(z);
+var m =[x,y,z];
+console.log(m);
+var lst=[1,"0",[1,2],4.8,true];
+console.log(lst);
+var obj={
+    name:"MR Naidu",
+    job:"developer",
+    age:19,
+
+
+}
+console.log(obj);
+let phone={
+    company:{
+        name:"samsung",
+        country: "korea",
+    },
+    model :"galaxy s10" ,
+    color :"black",
+};
+console.log(phone.company.name)
+//accessing object property using dot notation/*
+console.log(phone.model);
+//accessing nested object property using bracket notation/*
+console.log(phone["company"]["country"])
+let outcome=[[1,2,3,],
+             [4,5,6,],
+            [7,8,9]   ]
+            console.log(outcome[0][1]);
+            console.log(outcome[1][1]);
