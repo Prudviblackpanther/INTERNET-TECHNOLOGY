d7f67cc37165bbcf4bcfdc0886706cefb78f21f9
prudviblackpanther <prudvinaidum03@gmail.com>
2024-04-04 10:07:32 +0530

    conditions

---
+++ Javascript/conditions.js
@@ -0,0 +1,11 @@
+let pill;
+console.log(pill)
+if (pill=='red'){
+    console.log('reality')}
+    else i(pill=='blue')
+    {
+        console.log('stay in matrix')
+    }
+    else {
+        console.log('kill yourself')
+    }
